064cddf45748eb96128e9da0ce9a342aeb9c0ae6
OLGA <1986o2019@mail.ru>
2023-07-08 00:11:47 +0300

    Задача 2

---
+++ Example002_max2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите 3 числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (a > b & a > c)
+    Console.Write("max: " + a);
+else if (b > a & b > c)
+    Console.Write("max: " + b);
+else 
+    Console.WriteLine("max: " + c);
+
